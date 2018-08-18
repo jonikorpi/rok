bebe7ae7d0da7a3201b36fb0de3c52f8e7ff74be
Joni Korpi <me@jonikorpi.com>
2018-08-18 10:35:46 +0300

    Prototypes scrolly UI

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    const panelCount = 3;
+    const panelCount = 5;
 
     return (
       <div className="panels" style={{ "--panel-count": panelCount }}>
         {[...Array(panelCount)].map((value, index) => (
-          <article className="panel" style={{ "--order": index }}>
+          <article className="panel" key={index} style={{ "--order": index }}>
             <div className="scrollable">
-              <h1>panel</h1>
+              {[...Array(Math.floor(Math.random() * 1000))].map(() => (
+                <h1>{Math.random()}</h1>
+              ))}
             </div>
           </article>
         ))}
